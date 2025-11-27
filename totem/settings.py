e5d018ff37aa9d0a0ba19b1d8308d17c1de0dbff
Victor Hugo <victorhugobritocoelho@gmail.com>
2025-11-27 20:12:52 -0300

    Fix environment variable configuration for Railway

--- totem/settings.py
+++ totem/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ja=1ehhnr(pb%$6k@^6b6#$x$uvgtat&0k*=%%bjn%cyz7c!h'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(ja=1ehhnr(pb%$6k@^6b6#$x$uvgtat&0k*=%%bjn%cyz7c!h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,cuxrie.xyz').split(',')
 
 
 # Application definition
