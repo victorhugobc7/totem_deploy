f4d8515906c15bc468c49eca944bb78b3abae9f0
Victor Hugo <victorhugobritocoelho@gmail.com>
2025-12-03 12:07:54 -0300

    Add migration merge for conflicting 0007 migrations

---
+++ app/migrations/0008_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0007_alter_pet_nome_alter_petimagem_imagem'),
+        ('app', '0007_change_idade_to_choice'),
+    ]
+
+    operations = [
+    ]
